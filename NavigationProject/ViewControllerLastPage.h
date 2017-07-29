c56aa593c3a0acae2fa0e15faca582b40c80b396
Treinamento <treinamento@treinamento1-maquina17.local>
2017-07-29 11:48:12 -0300

    COMMIT inicial do NavigationPage

---
+++ NavigationProject/ViewControllerLastPage.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerLastPage.h
+//  NavigationProject
+//
+//  Created by Treinamento on 29/07/17.
+//  Copyright © 2017 Treinamento. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerLastPage : UIViewController
+
+@end
---
+++ NavigationProject/ViewControllerLastPage.m
@@ -0,0 +1,40 @@
+//
+//  ViewControllerLastPage.m
+//  NavigationProject
+//
+//  Created by Treinamento on 29/07/17.
+//  Copyright © 2017 Treinamento. All rights reserved.
+//
+
+#import "ViewControllerLastPage.h"
+
+@interface ViewControllerLastPage ()
+
+@end
+
+@implementation ViewControllerLastPage
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)VoltarTUDO:(id)sender {
+    [self.navigationController popToRootViewControllerAnimated:YES];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
